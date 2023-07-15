97250270530ef02862fac37af9dfd95ebaaf460a
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-15 10:07:46 -0400

    npm installed

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export function App() {
+  return (<div>
+      <h1>
+        Tic Tac Toe - <button>New</button>
+      </h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>)
+}
