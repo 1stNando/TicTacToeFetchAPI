a88784cb41bfea1c1817d6c3eed8652eec808edc
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-15 10:56:30 -0400

    Function to update state. Using the API to send request and make a new game.

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,30 @@ export function App() {
     console.log(`You hace clicked on row ${row} and column ${column}`)
   }
 
+  // Define a function to handle a new game fetch from API
+  async function handleNewGame() {
+    // Make a POST request to ask for a new game
+    const response = await fetch(
+      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json()
+
+      // Make that the new STATE!
+      setGame(newGame)
+    }
+  }
+
   return (
     <div>
       <h1>
-        Tic Tac Toe - <button>New</button>
+        Tic Tac Toe - <button onClick={handleNewGame}>New</button>
       </h1>
       <ul>
         <li onClick={() => handleClickCell(0, 0)}>{game.board[0][0]}</li>
