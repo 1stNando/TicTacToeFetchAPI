2d509dc697ed3020c10ba8178c2d1009a98f5b2e
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-17 13:52:01 -0400

    Fixed mistake

--- src/App.tsx
+++ src/App.tsx
@@ -17,26 +17,6 @@ export function App() {
     console.log(`You hace clicked on row ${row} and column ${column}`)
   }
 
-  // Define a function to handle a new game fetch from API
-  async function handleNewGame() {
-    // Make a POST request to ask for a new game
-    const response = await fetch(
-      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
-      {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-      }
-    )
-
-    if (response.ok) {
-      // Get the response as JSON
-      const newGame = await response.json()
-
-      // Make that the new STATE!
-      setGame(newGame)
-    }
-  }
-
   // Next, we need to "Create a new game" to get a "Game ID" so that we can register moves.
   // Create function to handleNewGame that uses the game API. New Game needs to be a POST.
   async function handleNewGame() {
