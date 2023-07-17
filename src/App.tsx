e2eca1f155f0147f7d6d7f2cc322abd2cfd908d9
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-17 13:50:51 -0400

    Created function to handleNewGame(), POST to API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export function App() {
-  // Make a state using data:
+  // Make a STATE using data:
   const [game, setGame] = useState({
     board: [
       [' ', ' ', ' '],
@@ -20,6 +20,26 @@ export function App() {
   // Define a function to handle a new game fetch from API
   async function handleNewGame() {
     // Make a POST request to ask for a new game
+    const response = await fetch(
+      'https://sdg-tic-tac-toe-api.herokuapp.com/game',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+
+    if (response.ok) {
+      // Get the response as JSON
+      const newGame = await response.json()
+
+      // Make that the new STATE!
+      setGame(newGame)
+    }
+  }
+
+  // Next, we need to "Create a new game" to get a "Game ID" so that we can register moves.
+  // Create function to handleNewGame that uses the game API. New Game needs to be a POST.
+  async function handleNewGame() {
     const response = await fetch(
       'https://sdg-tic-tac-toe-api.herokuapp.com/game',
       {
